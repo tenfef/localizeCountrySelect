6836e531133edc1faa7077dee882036c06b75244
Jordan <jordanclist@gmail.com>
2017-07-15 20:19:19 +1200

    Update localizeCountrySelect.js

--- localizeCountrySelect.js
+++ localizeCountrySelect.js
@@ -2,10 +2,11 @@ $.countryLocalizeSelect = jQuery.fn.countryLocalizeSelect = function () {
 	var $elems = this;
 
 	var country = null;
+	var authKey = null; // please sign up for a free IP Find account to add this
 
 	$.ajax({
 	  	dataType: "json",
-	  	url: 'https://ipfind.co/me',				  
+	  	url: 'https://ipfind.co/me?auth=' + authKey',
 	  	success: function(data){
     		if (data.country){
     			$elems.each(function(){
