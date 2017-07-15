ea318eaaed3ad8044b640f1285a3cffe37767df9
Jordan Clist <jordan@fergusapp.com>
2017-07-15 20:28:59 +1200

    update example

--- localizeCountrySelect.js
+++ localizeCountrySelect.js
@@ -1,33 +1,36 @@
-$.countryLocalizeSelect = jQuery.fn.countryLocalizeSelect = function () {
-	var $elems = this;
+$.countryLocalizeSelect = jQuery.fn.countryLocalizeSelect = function (authKey) {
+    var $elems = this;
 
-	var country = null;
+    var country = null;
+    if (! authKey){
+        console.warn("No auth key provided. please sign up for a free IP Find account at https://ipfind.co");
+    }
 
-	$.ajax({
-	  	dataType: "json",
-	  	url: 'https://ipfind.co/me',				  
-	  	success: function(data){
-    		if (data.country){
-    			$elems.each(function(){
-					var $el = $(this);
-					$el.val(data.country);
-				});
-    		}    		
-    	},
-    	error : function (error){
-    		var resp = error.responseText;
-    		if (resp){
-    			var errorObj = JSON.parse(resp);
-    			if (errorObj && errorObj.error){
-    				console.error("Could not localise select: ", errorObj.error);
-    				return;
-    			}
-    		} 
+    $.ajax({
+        dataType: "json",
+        url: 'https://ipfind.co/me?auth=' + authKey,
+        success: function(data){
+            if (data.country){
+                $elems.each(function(){
+                    var $el = $(this);
+                    $el.val(data.country);
+                });
+            }           
+        },
+        error : function (error){
+            var resp = error.responseText;
+            if (resp){
+                var errorObj = JSON.parse(resp);
+                if (errorObj && errorObj.error){
+                    console.error("Could not localise select: ", errorObj.error);
+                    return;
+                }
+            } 
 
-    		console.error("Could not localise select: ", resp);
-    					    		
-    	}
-	});	
+            console.error("Could not localise select: ", resp);
+                                    
+        }
+    }); 
 
-	return this;
+    return this;
 };
